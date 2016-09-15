b5f8316794dbe335fb0372bc1f9d28485c329df1
Zi-Yu Huang <zhhuang@caltech.edu>
2016-09-14 21:42:23 -0700

    Added new file

---
+++ ex4_3.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+import bootcamp_utils
+
+# write backtrack_steps() function
+def backtrack_steps():
+    """ computes the number of steps it takes for a random walker"""
+    step = 0
+    count = 0
+
+    while (step <= 0):
+        count += 1
+        if np.random.randint(0, 2) == 0:
+            step +=1
+        else:
+            step -=1
+
+    return count
+
+# generate 10000 of backtrack
+backtrack = np.empty(100)
+for i in range(100):
+    backtrack[i] = backtrack_steps()
+
+# plot histogram
+plt.figure(1)
+_ = plt.hist(backtrack, bins=100, normed=True)
+plt.xlabel('backtrack steps')
+plt.ylabel('PDF')
+#plt.show()
+
+# plot ECDF
+plt.figure(2)
+x_backtrack, y_backtrack = bootcamp_utils.ecdf(backtrack)
+plt.plot(x_backtrack, y_backtrack, marker='.', linestyle='none')
+plt.xscale('log')
+
+plt.show()
+
+# plot complementary cumulative distribution function
+#ccdf = c / t**(a + 1)
