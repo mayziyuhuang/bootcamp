1823f1d34f1a2cde2232f4448de488b66964669d
Zi-Yu Huang <zhhuang@caltech.edu>
2016-09-16 11:58:17 -0700

    fixed

---
+++ lesson42.py
@@ -0,0 +1,13 @@
+# comment line type jupyter notebook
+# it will open a website
+# click new
+# choose python conda root
+# type print('hello, world')
+# when you can to print it
+# use shift + enter
+
+# can download as hmtl
+# download and open up
+
+# can use Bokeh to make more interactive
+# but syntax is different
